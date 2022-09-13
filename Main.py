636410dea761cdd87bb6e8ca8dceb328aacb1ad9
antonypeter839 <110010533+antonypeter839@users.noreply.github.com>
2022-09-13 12:13:10 +0530

    try2.0

--- Main.py
+++ Main.py
@@ -1,7 +1,22 @@
 from typing import List
-
+def swap(arr,j,k):
+  temp=arr[j]
+  arr[j]=arr[k]
+  arr[k]=temp
+             
 def insertionSort(array) -> List[int]:
-  for sort is n(select):
+  for i in range(0,len(array)-1):
+    temp=i
+    execution=0
+    for j in range(i,-1,-1):
+      if execution==0:
+        if array[i]<array[j]:
+          swap(array,i,j)
+          execution=1
+       else:
+        else:
+          array[j+1]=array[j]
+  return array
 
 # data = [9, 5, 1, 4, 3]
 input_data = input()
