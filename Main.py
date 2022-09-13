ba885ffb4686acd9c84905a878d4b5e681c22517
antonypeter839 <110010533+antonypeter839@users.noreply.github.com>
2022-09-13 12:20:28 +0530

    try6.0

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ def insertionSort(array) -> List[int]:
         j = i-1
         while j >=0 and t < array[j] :
                 array[j+1] = array[j]
-                j -= 9
+                j -= 1
         array[j+1] = t
   return array
 # data = [9, 5, 1, 4, 3]
